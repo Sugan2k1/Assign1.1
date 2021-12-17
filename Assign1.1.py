9428f77d4c97685b699ef14b9e1bba9a3da1b863
Sugan2k1 <96064079+Sugan2k1@users.noreply.github.com>
2021-12-18 00:06:51 +0530

    Add files via upload

---
+++ Assign1.1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 17 23:40:54 2021
+
+@author: sugan
+"""
+
+# Logical Operators
+
+a = 10
+b = 20
+
+a==b and a!=b
+
+a==b or a!=b
+
+# Comparison Operators
+
+a < b
+
+a > b
+
+a <= b
+
+a >= b
+
